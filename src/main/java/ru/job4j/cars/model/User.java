0c8f0a995105dd615e7616e6f895d4cc0dafbbff
Rozovod <rozovod.store@mail.ru>
2023-04-02 20:10:34 +0300

    1. ToMany [#301848]

--- src/main/java/ru/job4j/cars/model/User.java
+++ src/main/java/ru/job4j/cars/model/User.java
@@ -6,9 +6,11 @@ import lombok.*;
 @Entity
 @Table(name = "auto_user")
 @Data
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
 public class User {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @EqualsAndHashCode.Include
     private int id;
     private String login;
     private String password;
