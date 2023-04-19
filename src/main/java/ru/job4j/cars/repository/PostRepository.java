7284aef1861ff5391d80002c33b9923cc6a969ae
Rozovod <rozovod.store@mail.ru>
2023-04-19 10:24:12 +0300

    1. Фильтры для объявлений машин [#4745 #410772]

--- src/main/java/ru/job4j/cars/repository/PostRepository.java
+++ src/main/java/ru/job4j/cars/repository/PostRepository.java
@@ -4,6 +4,7 @@ import lombok.AllArgsConstructor;
 import org.springframework.stereotype.Repository;
 import ru.job4j.cars.model.Post;
 
+import java.time.LocalDateTime;
 import java.util.List;
 import java.util.Map;
 
@@ -13,23 +14,21 @@ public class PostRepository {
     private final CrudRepository crudRepository;
 
     public List<Post> findFromLastDay() {
+        LocalDateTime yesterday = LocalDateTime.now().minusDays(1);
         return crudRepository.query(
-                "SELECT p FROM Post p JOIN FETCH p.user JOIN FETCH p.priceHistories "
-                        + "JOIN FETCH p.participates JOIN FETCH p.car JOIN FETCH p.files "
-                        + "WHERE p.created >= LocalDateTime.now().minusDays(1)", Post.class);
+                "SELECT p FROM Post p WHERE p.created >= :yesterday",
+                Post.class, Map.of("yesterday", yesterday));
     }
 
     public List<Post> findWithPhoto() {
         return crudRepository.query(
-                "SELECT p FROM Post p JOIN FETCH p.user JOIN FETCH p.priceHistories "
-                        + "JOIN FETCH p.participates JOIN FETCH p.car JOIN FETCH p.files "
+                "SELECT p FROM Post p JOIN FETCH p.files "
                         + "WHERE size(files) > 0", Post.class);
     }
 
     public List<Post> findByCarBrand(String brand) {
         return crudRepository.query(
-                "SELECT p FROM Post p JOIN FETCH p.user JOIN FETCH p.priceHistories "
-                        + "JOIN FETCH p.participates JOIN FETCH p.car JOIN FETCH p.files "
-                        + "WHERE p.car.name LIKE :brand", Post.class, Map.of("brand", brand));
+                "SELECT p FROM Post p JOIN FETCH p.car WHERE p.car.name LIKE :brand",
+                Post.class, Map.of("brand", brand));
     }
 }
